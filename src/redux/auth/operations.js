47800680a9ee861ff9f1ce66fcd76fb3ae4209fc
Ivan <ivandovhopol@gmail.com>
2024-03-23 04:10:46 +0200

    add host

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'http://localhost:3008/api/';
+axios.defaults.baseURL = 'https://node-js-hw-v61j.onrender.com/api/';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
