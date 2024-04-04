cb182e249f2805b8378bd31fe229d9ef6884bf98
IvanDovhopol <104319381+IvanDovhopol@users.noreply.github.com>
2024-04-04 17:50:30 +0000

    Deploying to gh-pages from @ IvanDovhopol/phonebook@32232fcd69ac31a56f90ea0f657d3ae66965bd48 🚀

---
+++ static/js/34.337cdc63.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook=self.webpackChunkphonebook||[]).push([[34],{3034:(e,n,o)=>{o.r(n),o.d(n,{default:()=>c});o(5043);var s=o(3216),t=o(579);function c(){const e=(0,s.Zp)();return(0,t.jsxs)("div",{children:[(0,t.jsx)("p",{children:"Sorry, this page does not exist"}),(0,t.jsx)("button",{onClick:()=>e(-1),children:"Go back."})]})}}}]);
+//# sourceMappingURL=34.337cdc63.chunk.js.map
